fc87ce0642f2fdb48f4f3900cf6b0bb9ae4ab681
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-02-24 14:45:04 +0800

    Added Wrap2D script

---
+++ Game-Development/Assets/Scripts/Wrap2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wrap2D : MonoBehaviour {
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (transform.position.x <= -2.95f) {
+
+						transform.position = new Vector3 (2.95f, transform.position.y, transform.position.z);
+		
+				} 
+		else if (transform.position.x >= 2.95f) {
+				
+						transform.position = new Vector3 (-2.95f, transform.position.y, transform.position.z);
+				}
+	
+	}
+}
