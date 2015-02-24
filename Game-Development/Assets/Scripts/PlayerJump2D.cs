afb09bf9808ca88963e7d555052f2c3b613f46ad
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-02-24 15:01:13 +0800

    Added Jump2D script(Grounded)

---
+++ Game-Development/Assets/Scripts/PlayerJump2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump2D : MonoBehaviour {
+
+	public bool grounded;
+	public float jumpheight = 500f;
+
+	public Transform groundcheck;
+
+	float groundRadius = .2f;
+	public LayerMask ground;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		grounded = Physics2D.OverlapCircle (groundcheck.position, groundRadius, ground);
+
+	}
+}
