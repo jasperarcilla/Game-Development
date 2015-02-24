3223bab983bd68cd1475d895a20f7997a6f988ff
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-02-24 14:15:10 +0800

    Added PlayerControl script

---
+++ Game-Development/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+
+	public float moveSpeed = 4f; 
+	// Use this for initialization 
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float h = Input.GetAxis ("Horizontal");  
+
+		rigidbody2D.velocity = new Vector2 (h * moveSpeed, rigidbody2D.velocity.y); 
+
+	}
+}
