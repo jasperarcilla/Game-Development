de31aa55c976440af3fdf9605c44a09fe8016ebb
jasperarcilla <jasperarcilla@rocketmail.com>
2015-03-07 13:10:44 +0800

    added highscore

--- Game-Development/Assets/Scripts/OnGUI2D.cs
+++ Game-Development/Assets/Scripts/OnGUI2D.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class OnGUI2D : MonoBehaviour {
 
 	public static int score;
+	int highscore;
 	// Use this for initialization
 	void Start () {
 		score = 0;
 	}
 	void OnGUI(){
 		GUI.Label(new Rect(10,10,100,20),"score: " + score);
+		GUI.Label (new Rect (10, 30, 100, 20), "highscore: " + highscore);
 	}
 }
