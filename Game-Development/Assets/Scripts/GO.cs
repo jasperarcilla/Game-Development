65e52bc7269a13d93239faade5a8e37741b58ade
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-03-10 16:26:54 +0800

    Added some changes

---
+++ Game-Development/Assets/Scripts/GO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GO : MonoBehaviour {
+
+	void OnCollision2DEnter(Collision2D col){
+		if (col.gameObject.name == "TempTerrain") {
+			SendMessage("Fuck");
+		}
+	}
+	}
