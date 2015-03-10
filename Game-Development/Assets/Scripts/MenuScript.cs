ccfba10ad84dc0dd92c393743f61d90049b33daa
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-03-11 00:09:23 +0800

    Fixed menu design

--- Game-Development/Assets/Scripts/MenuScript.cs
+++ Game-Development/Assets/Scripts/MenuScript.cs
@@ -7,14 +7,14 @@ public class MenuScript : MonoBehaviour
 {
 	void OnGUI()
 	{
-		const int buttonWidth = 84;
-		const int buttonHeight = 60;
+		const int buttonWidth = 70;
+		const int buttonHeight = 50;
 		
 		// Determine the button's place on screen
 		// Center in X, 2/3 of the height in Y
 		Rect buttonRect = new Rect(
 			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 3) - (buttonHeight / 2),
+			(2 * Screen.height / 2.25f) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			);
