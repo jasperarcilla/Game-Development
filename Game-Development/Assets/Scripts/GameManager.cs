15104f185f0b1161ec3104e3293990a6b54db544
Lemuel Castro <xlemuel14@gmail.com>
2015-03-10 20:33:34 +0800

    Added Game Over Scene

--- Game-Development/Assets/Scripts/GameManager.cs
+++ Game-Development/Assets/Scripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour {
 			if(playerHeightY < (currentCameraHeight - 5.25f))
 			{
 				OnGUI2D.OG2D.checkhighscore();
-				Application.LoadLevel(Application.loadedLevel);
+				Application.LoadLevel("GameOver");
 			}
 		}
 	if (playerHeightY > OnGUI2D.score) {
