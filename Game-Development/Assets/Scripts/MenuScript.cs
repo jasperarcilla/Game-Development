517e0cca499fd125f29f0c2b8811dbf6475b36b4
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-03-12 16:55:51 +0800

    Menu design

--- Game-Development/Assets/Scripts/MenuScript.cs
+++ Game-Development/Assets/Scripts/MenuScript.cs
@@ -5,22 +5,31 @@
 /// </summary>
 public class MenuScript : MonoBehaviour
 {
+	private GUISkin skin;
+
+	void Start()
+	{
+		skin = Resources.Load ("GUISkin") as GUISkin;
+	}
+
 	void OnGUI()
 	{
-		const int buttonWidth = 70;
-		const int buttonHeight = 50;
+		const int buttonWidth = 139;
+		const int buttonHeight = 42;
+
+		GUI.skin = skin;
 		
 		// Determine the button's place on screen
 		// Center in X, 2/3 of the height in Y
 		Rect buttonRect = new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 6.3f) - (buttonHeight / 2),
+			Screen.width / 2 - (buttonWidth / 1.8f),
+			(2 * Screen.height / 3.5f) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			);
 		
 		// Draw a button to start the game
-		if(GUI.Button(buttonRect,"Start!"))
+		if(GUI.Button(buttonRect," "))
 		{
 			// On Click, load the first level.
 			// "Stage1" is the name of the first scene we created.
