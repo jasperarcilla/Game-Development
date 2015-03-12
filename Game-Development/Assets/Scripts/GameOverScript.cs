c54335ccde5ce23f0a8ebe2968f396738d46bdec
vom12 <vincentoscar.maceda@bicol-u.edu.ph>
2015-03-12 23:18:05 +0800

    Finalized GameOver scence

--- Game-Development/Assets/Scripts/GameOverScript.cs
+++ Game-Development/Assets/Scripts/GameOverScript.cs
@@ -5,28 +5,48 @@
 /// </summary>
 public class GameOverScript : MonoBehaviour
 {
+	private GUISkin skin;
+	private GUISkin skin1;
+
+	void Start()
+	{
+		skin = Resources.Load ("GUISkin2") as GUISkin;
+		skin1 = Resources.Load ("GUISkin3") as GUISkin;
+	}
+
 	void OnGUI()
 	{
-		const int buttonWidth = 120;
-		const int buttonHeight = 60;
-		
+		const int buttonWidth = 139;
+		const int buttonHeight = 42;
+
+		GUI.skin = skin;
+
 		if (
 			GUI.Button(
 			// Center in X, 1/3 of the height in Y
 			new Rect(
 			Screen.width / 2 - (buttonWidth / 2),
-			(1 * Screen.height / 3) - (buttonHeight / 2),
+			(1 * Screen.height / 2) - (buttonHeight / 2),
 			buttonWidth,
 			buttonHeight
 			),
-			"Retry!"
+			" "
 			)
 			)
 		{
 			// Reload the level
 			Application.LoadLevel("Jumper");
 		}
-		
+		Menu ();
+
+	}
+	void Menu()
+	{
+		const int buttonWidth = 139;
+		const int buttonHeight = 42;
+
+		GUI.skin = skin1;
+
 		if (
 			GUI.Button(
 			// Center in X, 2/3 of the height in Y
@@ -36,7 +56,7 @@ public class GameOverScript : MonoBehaviour
 			buttonWidth,
 			buttonHeight
 			),
-			"Back to menu"
+			" "
 			)
 			)
 		{
