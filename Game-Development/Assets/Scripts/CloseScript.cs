20a9c85e06902db97456f29c2bc90200dd4b085e
jasper arcilla <jasperarcilla@rocketmail.com>
2015-03-13 09:43:19 +0800

    Close button

---
+++ Game-Development/Assets/Scripts/CloseScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class CloseScript : MonoBehaviour
+{
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load ("GUISkin4") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 50;
+		const int buttonHeight = 50;
+		
+		GUI.skin = skin;
+		
+		// Determine the button's place on screen
+		// Center in X, 2/3 of the height in Y
+		Rect buttonRect = new Rect(
+			Screen.width / 1.025f - (buttonWidth / 1.8f),
+			(2 * Screen.height / 35) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+		
+		// Draw a button to start the game
+		if(GUI.Button(buttonRect," "))
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.Quit();
+		}
+	}
+}
